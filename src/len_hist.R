6a900906cd647f18e07d1c21608fe9a8c3a753da
Artem <artem@localhost.localdomain>
2021-06-08 16:06:38 +0300

    histogram of the intersection

--- src/len_hist.R
+++ src/len_hist.R
@@ -7,6 +7,7 @@ source('lib.R')
 #NAME <- 'H3K36me3_SK-N-SH.ENCFF040UTU.hg38'
 #NAME <- 'H3K36me3_SK-N-SH.ENCFF815MPJ.hg19'
 #NAME <- 'H3K36me3_SK-N-SH.ENCFF815MPJ.hg38'
+NAME <- 'H3K36me3_SK-N-SH.intersect_with_G4.bed'
 
 ###
 
@@ -14,6 +15,11 @@ bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALS
 #colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
+bed_df %>%
+  arrange(-len) %>%
+  head
+  
+
 
 ggplot(bed_df) +
   aes(x = len) +
